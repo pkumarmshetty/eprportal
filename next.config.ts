4b46c42290c7d58267a08c09dc7c483124d80efb
Tejash JL <jl.tejash@gmail.com>
2025-07-19 17:03:18 +0530

    update metadata for EPR Portal and adjust start script port

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false,
-  /* config options here */
+    reactStrictMode: false,
+    /* config options here */
     async rewrites() {
         return [
             {
