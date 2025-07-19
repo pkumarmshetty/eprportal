f20061f76eca40d67eed1f068ecb37bdd11f9468
Tejash JL <jl.tejash@gmail.com>
2025-07-19 17:31:44 +0530

    configure Next.js to ignore TypeScript and ESLint errors during build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: false,
+    typescript: {
+        // This skips type-checking during the build
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        // Optional: skip ESLint errors during build too
+        ignoreDuringBuilds: true,
+    },
     /* config options here */
     async rewrites() {
         return [
