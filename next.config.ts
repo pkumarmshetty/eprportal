a13521337014a7be0aa90295b42601fb8460d49b
Tejash JL <jl.tejash@gmail.com>
2025-07-19 12:25:07 +0530

    add rewrites for admin dashboard routing and update .gitignore

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: '/admin/dashboard',
+                destination: '/dashboard.html',
+            },
+        ];
+    },
 };
 
 export default nextConfig;
